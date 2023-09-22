d2d2366f227a6aa79995541d5d955978816f8a9b
Jane-547 <Jane-547@bk.com>
2023-09-22 18:17:13 +0300

    Task 04 added

--- HW001/src/Task03/Printer.java
+++ HW001/src/Task03/Printer.java
@@ -1,4 +1,53 @@
-package Task03;
+class Calculator {
+    static int calculate(char op, int a, int b) {
+        // Введите свое решение ниже
+        int res = 0;
+        switch (op) {
 
+            case '+':
+                res = a + b;
+                break;
+
+            case '-':
+                res = a - b;
+                break;
+
+            case '/':
+                res = a / b;
+                break;
+
+            case '*':
+                res = a * b;
+                break;
+
+            default:
+                System.out.println("Некорректный оператор: " + op);
+                break;
+        }
+        return res;
+    }
+}
+
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
 public class Printer {
+    public static void main(String[] args) {
+        int a = 0;
+        char op = ' ';
+        int b = 0;
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            a = 11;
+            op = ';';
+            b = 5;
+        } else {
+            a = Integer.parseInt(args[0]);
+            op = args[1].charAt(0);
+            b = Integer.parseInt(args[2]);
+        }
+
+        Calculator calculator = new Calculator();
+        int result = calculator.calculate(op, a, b);
+        System.out.println(result);
+    }
 }
