c2b2a8ab9b25409cba9a3a8e9797242a7e038ed9
Jane-547 <Jane-547@bk.com>
2023-09-23 16:11:44 +0300

    Task 04 modified

--- HW001/src/Task04/Printer.java
+++ HW001/src/Task04/Printer.java
@@ -1,10 +1,8 @@
 package Task04;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 class Equation {
     static String getSolution(String str) {
@@ -20,21 +18,29 @@ class Equation {
                 String first = parts[0].replace('?', pos);
                 q = Integer.parseInt(first);
             } else q = Integer.parseInt(parts[0]);
-            //System.out.println(q);
+
             for (int j = 0; j < 10; j++) {
                 if (parts[1].contains("?")) {
                     char pos1 = Integer.toString(j).charAt(0);
                     String second = parts[1].replace('?', pos1);
                     w = Integer.parseInt(second);
                 } else w = Integer.parseInt(parts[1]);
-                //System.out.println(w);
+
+                int count = 0;
+
                 for (int k = 0; k < 10; k++) {
                     if (parts[2].contains("?")) {
                         char pos2 = Integer.toString(k).charAt(0);
                         String third = parts[2].replace('?', pos2);
                         e = Integer.parseInt(third);
-                    } else e = Integer.parseInt(parts[2]);
-                    if (q + w == e) res = "Result: " + q + " + " + w + " = " + e;
+                    } else {
+                        e = Integer.parseInt(parts[2]);
+                        count++;
+                    }
+                    if (q + w == e && count < 2) {
+                        res = "Result: " + q + " + " + w + " = " + e;
+                        System.out.println(res);
+                    }
                 }
             }
         }
@@ -45,16 +51,28 @@ class Equation {
 // Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
         class Printer {
             public static void main(String[] args) {
+
                 String file = "";
 
-                if (args.length == 0) {
-                    file = "2? + ?5 = 69";
-                } else {
-                    file = args[0];
+                try {
+                    Scanner sc = new Scanner(new File("C:\\Всякое мое\\Учеба Гик\\5. Java\\Java_Homeworks\\HW001\\src\\Task04\\input.txt"));
+                    while (sc.hasNextLine()) {
+                        file = sc.nextLine();
+                    }
+                    sc.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
                 }
+//                String file = "";
+//
+//                if (args.length == 0) {
+//                    file = "input.txt";
+//                } else {
+//                    file = args[0];
+//                }
 
                 Equation eq = new Equation();
                 String result = eq.getSolution(file);
-                System.out.println(result);
+                //System.out.println(result);
             }
         }
